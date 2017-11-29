35adc04bbfbf697804a41d95868ad9ffb37473da
Tommy Gunnarsson <tommy.gunnarsson@klarna.com>
2017-11-29 22:52:45 +0100

    Added base template.

--- mepy/views.py
+++ mepy/views.py
@@ -1,13 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from mepy.CircullumVitae import CircullumVitae
 from mepy.About import About
 
+
 def index(request):
-    return HttpResponse("index")
+    return render(request, 'base.html')
+
 
 def circullumVitae(request):
     return CircullumVitae().respond()
+
 
 def aboutMe(request):
     return About().respond()
